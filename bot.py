225c49e89cc1bac42ed4c3c5fcf523afc2bb53a5
hydrastro <9256943+hydrastro@users.noreply.github.com>
2021-05-15 00:03:49 +0200

    code cleanup

--- bot.py
+++ bot.py
@@ -1,26 +1,26 @@
-import config
 import json
 import random
 import re
-import requests
 import sqlite3
+import requests
+import config
 
-base = "https://api.telegram.org/bot{}/".format(config.token)
+BASE = "https://api.telegram.org/bot{}/".format(config.token)
 con = sqlite3.connect('messages.db')
-con.create_function('regexp', 2, lambda x, y: 1 if re.search(x,y) else 0)
+con.create_function('regexp', 2, lambda x, y: 1 if re.search(x, y) else 0)
 
-def get_updates(offset = None):
-    url = base + "getUpdates?timeout=50"
+def get_updates(offset=None):
+    url = BASE + "getUpdates?timeout=50"
     if offset:
         url = url + "&offset={}".format(offset + 1)
-    r = requests.get(url)
-    return json.loads(r.content)
+    req = requests.get(url)
+    return json.loads(req.content)
 
 def send_message(message, chat_id):
-    url = base + "sendMessage?chat_id={}&text={}".format(chat_id, message)
+    url = BASE + "sendMessage?chat_id={}&text={}".format(chat_id, message)
     if message is not None:
-        r = requests.get(url)
-        return json.loads(r.content)
+        req = requests.get(url)
+        return json.loads(req.content)
     return None
 
 def get_previous_message_containing(message_info, string):
@@ -29,7 +29,7 @@ def get_previous_message_containing(message_info, string):
     cur = con.cursor()
     query = "SELECT MessageID, SenderName, SenderID, ChatID, Message, ReplyToMessageID FROM messages WHERE Message REGEXP ? AND ChatID = ? "
     parameters = (string, chat_id)
-    if reply_to_id != None:
+    if reply_to_id is not None:
         query += "AND MessageID = ? "
         parameters += (reply_to_id,)
     query += "ORDER BY MessageID DESC"
@@ -61,17 +61,17 @@ def get_reply(message_info):
         if message.startswith(".roulette"):
             return get_roulette()
         if message == ".roll":
-            return random.randint(0,10)
+            return random.randint(0, 10)
         if message.startswith("rand"):
-            message = message[4:];
+            message = message[4:]
             if message.startswith("(") and message.endswith(")"):
                 message = message[1:-1]
                 message.replace(" ", "")
                 split = message.split(",", 1)
-                min = split[0]
-                max = split[1]
-                if(min <= max):
-                    return random.randint(int(min), int(max))
+                min_rand = split[0]
+                max_rand = split[1]
+                if min_rand <= max_rand:
+                    return random.randint(int(min_rand), int(max_rand))
         if "!leaf" in message_lowercase or "!canadian" in message_lowercase:
             return "🇨🇦"
         if "/thread" in message_lowercase:
@@ -95,61 +95,61 @@ def get_reply(message_info):
                 return None
             old = split[1]
             new = split[2]
-            is_valid = False
             try:
-                print(old)
                 re.compile(old)
             except re.error:
                 return None
             reply_info = get_previous_message_containing(message_info, old)
-            if reply_info == None:
+            if reply_info is None:
                 return None
             max_replace = 1
             if replace_all:
                 max_replace = len(reply_info[4])
-            if reply_info != None:
+            if reply_info is not None:
                 try:
                     print(old + "\n" + new)
                     reply = re.sub(old, new, reply_info[4], max_replace)
                     reply = "<" + reply_info[1] + ">: " + reply
                 except:
-                    return None
+                    return reply
     return reply
 
 def start_bot():
     update_id = None
     while True:
-        updates = get_updates(offset = update_id)
+        updates = get_updates(offset=update_id)
         if "result" in updates:
             updates = updates["result"]
         else:
             continue
-        if updates:
-            for item in updates:
-                update_id = item["update_id"]
-                try:
-                    message = str(item["message"]["text"])
-                except:
-                    message = None
-                if message != None:
-                    message_id = item["message"]["message_id"]
-                    sender_name = item["message"]["from"]["first_name"]
-                    sender_id = item["message"]["from"]["id"]
-                    chat_id = item["message"]["chat"]["id"]
-                    reply_to_message_id = None
-                    if "reply_to_message" in item["message"]:
-                        reply_to_message_id = item["message"]["reply_to_message"]["message_id"]
-                    current_message = {"message_id": message_id, "sender_name": sender_name, "sender_id": sender_id, "chat_id": chat_id, "message": message, "reply_to_message_id": reply_to_message_id}
-                    reply = get_reply(current_message)
-                    cur = con.cursor()
-                    con.execute("INSERT INTO messages (MessageID, SenderName, SenderID, ChatID, Message, ReplyToMessageID) VALUES (?, ?, ?, ?, ?, ?)", (message_id, sender_name, sender_id, chat_id, message, reply))
-                    con.commit()
-                    if reply != None:
-                        sent_message = send_message(reply, chat_id)
-                        if sent_message != None and "result" in sent_message:
-                            sent_message_id = sent_message["result"]["message_id"]
-                            sent_message_sender_name = sent_message["result"]["from"]["first_name"]
-                            sent_message_sender_id = sent_message["result"]["from"]["id"]
-                            con.execute("INSERT INTO messages (MessageID, SenderName, SenderID, ChatID, Message) VALUES (?, ?, ?, ?, ?)", (sent_message_id, sent_message_sender_name, sent_message_sender_id, chat_id, reply))
-                            con.commit()
+        if not updates:
+            continue
+        for item in updates:
+            update_id = item["update_id"]
+            try:
+                message = str(item["message"]["text"])
+            except:
+                continue
+            if message is None:
+                continue
+            message_id = item["message"]["message_id"]
+            sender_name = item["message"]["from"]["first_name"]
+            sender_id = item["message"]["from"]["id"]
+            chat_id = item["message"]["chat"]["id"]
+            reply_to_message_id = None
+            if "reply_to_message" in item["message"]:
+                reply_to_message_id = item["message"]["reply_to_message"]["message_id"]
+            current_message = {"message_id": message_id, "sender_name": sender_name, "sender_id": sender_id, "chat_id": chat_id, "message": message, "reply_to_message_id": reply_to_message_id}
+            reply = get_reply(current_message)
+            con.execute("INSERT INTO messages (MessageID, SenderName, SenderID, ChatID, Message, ReplyToMessageID) VALUES (?, ?, ?, ?, ?, ?)", (message_id, sender_name, sender_id, chat_id, message, reply))
+            con.commit()
+            if reply is None:
+                continue
+            sent_message = send_message(reply, chat_id)
+            if sent_message is not None and "result" in sent_message:
+                sent_message_id = sent_message["result"]["message_id"]
+                sent_message_sender_name = sent_message["result"]["from"]["first_name"]
+                sent_message_sender_id = sent_message["result"]["from"]["id"]
+                con.execute("INSERT INTO messages (MessageID, SenderName, SenderID, ChatID, Message) VALUES (?, ?, ?, ?, ?)", (sent_message_id, sent_message_sender_name, sent_message_sender_id, chat_id, reply))
+                con.commit()
 start_bot()
