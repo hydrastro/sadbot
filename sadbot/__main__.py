b49defb13ee35749779b3ba18f407a28c1adcef6
amo-incognito <>
2021-05-18 21:28:16 +0200

    Blackify code

--- sadbot/__main__.py
+++ sadbot/__main__.py
@@ -12,7 +12,15 @@ import os
 class Message:
     """message class"""
 
-    def __init__(self, message_id: int = 0, sender_name: str = "", sender_id: int = 0, chat_id: int = 0, text: str = "", reply_to_message_id: int = 0) -> None:
+    def __init__(
+        self,
+        message_id: int = 0,
+        sender_name: str = "",
+        sender_id: int = 0,
+        chat_id: int = 0,
+        text: str = "",
+        reply_to_message_id: int = 0,
+    ) -> None:
         self.messageID = message_id
         self.senderName = sender_name
         self.senderID = sender_id
@@ -25,20 +33,21 @@ class App:
     """main app class. when called it starts the bot"""
 
     def __init__(self) -> None:
-        self.DB = sqlite3.connect('messages.db')
-        self.DB.create_function('regexp', 2, lambda x,
-                                y: 1 if re.search(x, y) else 0)
+        self.DB = sqlite3.connect("messages.db")
+        self.DB.create_function("regexp", 2, lambda x, y: 1 if re.search(x, y) else 0)
         token = config.token
         if os.getenv("TOKEN"):
             token = os.getenv("TOKEN")
         self.baseURL = "https://api.telegram.org/bot{}/".format(token)
-        self.DB.execute("CREATE TABLE IF NOT EXISTS messages (    MessageID        integer,\
+        self.DB.execute(
+            "CREATE TABLE IF NOT EXISTS messages (    MessageID        integer,\
     SenderName       text,\
     SenderID         int,\
     ChatID           integer,\
     Message          text,\
     ReplyToMessageID int\
-)")
+)"
+        )
         self.startBot()
 
     def getUpdates(self, offset=None):
@@ -53,9 +62,9 @@ class App:
     def sendMessage(self, message: Message):
         """sends message to some chat using api"""
 
-        url = self.baseURL + \
-            "sendMessage?chat_id={}&text={}".format(
-                message.chatID, message.messageText)
+        url = self.baseURL + "sendMessage?chat_id={}&text={}".format(
+            message.chatID, message.messageText
+        )
         if message is not None:
             req = requests.get(url)
             return json.loads(req.content)
@@ -65,8 +74,10 @@ class App:
         """retrieves a previous message from the database matching a certain regex pattern"""
 
         cur = self.DB.cursor()
-        query = "SELECT MessageID, SenderName, SenderID, ChatID, Message, " \
+        query = (
+            "SELECT MessageID, SenderName, SenderID, ChatID, Message, "
             "ReplyToMessageID FROM messages WHERE Message REGEXP ? AND ChatID = ? "
+        )
         params = (reg, message.chatID)
         if message.replyID is not None:
             query += "AND MessageID = ? "
@@ -74,22 +85,38 @@ class App:
         query += "ORDER BY MessageID DESC"
         cur.execute(query, params)
         data = cur.fetchone()
-        mes = Message(message_id=data[0], sender_name=data[1], sender_id=data[2],
-                      chat_id=data[3], text=data[4], reply_to_message_id=data[5])
+        mes = Message(
+            message_id=data[0],
+            sender_name=data[1],
+            sender_id=data[2],
+            chat_id=data[3],
+            text=data[4],
+            reply_to_message_id=data[5],
+        )
         return mes
 
     def randomInsult(self) -> str:
         """gets a reply for when the bot receives an insult"""
 
-        insult_replies = ["no u", "take that back", "contribute to make me better",
-                          "stupid human", "sTuPiD bOt1!1", "lord, have mercy: they don't know that they're saying."]
+        insult_replies = [
+            "no u",
+            "take that back",
+            "contribute to make me better",
+            "stupid human",
+            "sTuPiD bOt1!1",
+            "lord, have mercy: they don't know that they're saying.",
+        ]
         return random.choice(insult_replies)
 
     def randomCompliment(self) -> str:
         """gets a reply for when the bot receives a compliment"""
 
-        compliment_replies = ["t-thwanks s-senpaii *starts twerking*", "at your service, sir",
-                              "thank youu!!", "good human"]
+        compliment_replies = [
+            "t-thwanks s-senpaii *starts twerking*",
+            "at your service, sir",
+            "thank youu!!",
+            "good human",
+        ]
         return random.choice(compliment_replies)
 
     def getRoulette(self) -> str:
@@ -103,7 +130,9 @@ class App:
         """closes a discussion"""
 
         closed_thread_replies = [
-            "rekt", "*This thread has been archived at RebeccaBlackTech*"]
+            "rekt",
+            "*This thread has been archived at RebeccaBlackTech*",
+        ]
         return random.choice(closed_thread_replies)
 
     def getRandCommand(self, message: Message) -> str:
@@ -149,8 +178,7 @@ class App:
             max_replace = len(reply_message.messageText)
         if reply_message is not None:
             try:
-                reply = re.sub(
-                    old, new, reply_message.messageText, max_replace)
+                reply = re.sub(old, new, reply_message.messageText, max_replace)
                 reply = "<" + reply_message.senderName + ">: " + reply
                 return reply
             except re.error:
@@ -163,7 +191,7 @@ class App:
         text_lowercase = text.lower()
         if text is None:
             return None
-        if re.fullmatch(re.compile('s/.*/.*[/g]*'), text):
+        if re.fullmatch(re.compile("s/.*/.*[/g]*"), text):
             return self.getSedCommand(message)
         if text_lowercase.startswith(".roulette"):
             return self.getRoulette()
@@ -183,10 +211,21 @@ class App:
 
     def insertMessage(self, message: Message) -> None:
         """inserts a message into the database"""
-        query = "INSERT INTO messages (MessageID, SenderName, SenderID, ChatID, Message, " \
+        query = (
+            "INSERT INTO messages (MessageID, SenderName, SenderID, ChatID, Message, "
             "ReplyToMessageID) VALUES (?, ?, ?, ?, ?, ?)"
-        self.DB.execute(query, (message.messageID, message.senderName,
-                        message.senderID, message.chatID, message.messageText, message.replyID))
+        )
+        self.DB.execute(
+            query,
+            (
+                message.messageID,
+                message.senderName,
+                message.senderID,
+                message.chatID,
+                message.messageText,
+                message.replyID,
+            ),
+        )
         self.DB.commit()
 
     def startBot(self) -> None:
@@ -214,9 +253,17 @@ class App:
                 chat_id = item["message"]["chat"]["id"]
                 reply_to_message_id = None
                 if "reply_to_message" in item["message"]:
-                    reply_to_message_id = item["message"]["reply_to_message"]["message_id"]
-                message = Message(message_id, sender_name,
-                                  sender_id, chat_id, text, reply_to_message_id)
+                    reply_to_message_id = item["message"]["reply_to_message"][
+                        "message_id"
+                    ]
+                message = Message(
+                    message_id,
+                    sender_name,
+                    sender_id,
+                    chat_id,
+                    text,
+                    reply_to_message_id,
+                )
                 reply = self.getReply(message)
                 self.insertMessage(message)
                 if reply is None:
@@ -225,10 +272,18 @@ class App:
                 sent_message = self.sendMessage(new_message)
                 if sent_message is not None and "result" in sent_message:
                     sent_message_id = sent_message["result"]["message_id"]
-                    sent_message_sender_name = sent_message["result"]["from"]["first_name"]
+                    sent_message_sender_name = sent_message["result"]["from"][
+                        "first_name"
+                    ]
                     sent_message_sender_id = sent_message["result"]["from"]["id"]
-                    message = Message(sent_message_id, sent_message_sender_name,
-                                      sent_message_sender_id, message.chatID, reply, None)
+                    message = Message(
+                        sent_message_id,
+                        sent_message_sender_name,
+                        sent_message_sender_id,
+                        message.chatID,
+                        reply,
+                        None,
+                    )
                     self.insertMessage(message)
 
 
