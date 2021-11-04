db7cb7a49258cf00a8512ac3d96ef6f183ddd4e6
meguminloli <meguminloli@protonmail.com>
2021-11-04 17:11:26 +0200

    add dependencies to poetry and use flakes

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Application packaged using poetry2nix";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
+
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+    {
+      # Nixpkgs overlay providing the application
+      overlay = nixpkgs.lib.composeManyExtensions [
+        poetry2nix.overlay
+        (final: prev: {
+          # The application
+          sadbot = prev.poetry2nix.mkPoetryApplication {
+            projectDir = ./.;
+          };
+        })
+      ];
+    } // (flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ self.overlay ];
+        };
+      in
+      rec {
+        apps = {
+          sadbot = pkgs.sadbot;
+        };
+
+        defaultApp = apps.sadbot;
+      }));
+}
