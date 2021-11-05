7ced4aee0a0e9738c187abefb22b83e7e54712f5
hydrastro <9256943+hydrastro@users.noreply.github.com>
2021-11-05 17:07:13 +0100

    Cleaning things up a bit: moving warn tables into a new class ad hoc

--- sadbot/commands/warn.py
+++ sadbot/commands/warn.py
@@ -7,15 +7,22 @@ from sadbot.bot_action import BOT_ACTION_TYPE_REPLY_TEXT, BotAction
 from sadbot.command_interface import BOT_HANDLER_TYPE_MESSAGE, CommandInterface
 from sadbot.message import Message
 from sadbot.message_repository import MessageRepository
+from sadbot.classes.user_warnings import UserWarnings
 
 
 class WarnBotCommand(CommandInterface):
     """This is the warn command class"""
 
-    def __init__(self, app: App, message_repository: MessageRepository):
+    def __init__(
+        self,
+        app: App,
+        message_repository: MessageRepository,
+        user_warnings: UserWarnings,
+    ):
         """Initializes the warn command"""
         self.app = app
         self.message_repository = message_repository
+        self.user_warnings = user_warnings
 
     @property
     def handler_type(self) -> int:
@@ -27,9 +34,7 @@ class WarnBotCommand(CommandInterface):
         """Returns the regex string that triggers this command"""
         return "([.]|[!]|[/])[wW][aA][rR][nN]"
 
-    def get_reply(
-        self, message: Optional[Message] = None
-    ) -> Optional[List[BotAction]]:
+    def get_reply(self, message: Optional[Message] = None) -> Optional[List[BotAction]]:
         # pylint: disable=R0911
         """Returns the command output"""
         if message is None or message.text is None:
@@ -66,16 +71,16 @@ class WarnBotCommand(CommandInterface):
             return [
                 BotAction(
                     BOT_ACTION_TYPE_REPLY_TEXT,
-                    reply_text="You don't have enough rights to mute, kiddo.",
+                    reply_text="You don't have enough rights to warn, kiddo.",
                 )
             ]
         timestamp = int(time.time()) - 604800
-        count = self.message_repository.get_warns_since_timestamp(
+        count = self.user_warnings.get_warns_since_timestamp(
             message.chat_id, warn_sender_id, timestamp
         )
         reply_text = f"""{warn_username} has been successfully warned.
-        The user has {count} {'warns' if count != 1 else 'warn'} in the last week"""
-        self.message_repository.insert_new_warn(
+The user has been warned {count} {'times' if count != 1 else 'time'} in the last week"""
+        self.user_warnings.insert_new_warn(
             message.chat_id, warn_sender_id, int(time.time())
         )
         return [BotAction(BOT_ACTION_TYPE_REPLY_TEXT, reply_text=reply_text)]
--- sadbot/message_repository.py
+++ sadbot/message_repository.py
@@ -59,17 +59,6 @@ def get_bot_triggers_table_creation_query() -> str:
     """
 
 
-def get_warn_table_creation_query() -> str:
-    """Returns the warn table creation query"""
-    return """
-    CREATE TABLE IF NOT EXISTS warn_table (
-      ChatID     int,
-      UserID     int,
-      WarnDate   int
-    )
-    """
-
-
 class MessageRepository:  # pylint: disable=R0904
     """This class handles the messages database"""
 
@@ -80,7 +69,6 @@ class MessageRepository:  # pylint: disable=R0904
         self.con.execute(get_messages_table_creation_query())
         self.con.execute(get_usernames_table_creation_query())
         self.con.execute(get_bot_triggers_table_creation_query())
-        self.con.execute(get_warn_table_creation_query())
         self.heal_database()
 
     def heal_database(self) -> None:
@@ -116,40 +104,6 @@ class MessageRepository:  # pylint: disable=R0904
         message_time = datetime.datetime.utcnow().timestamp() - time
         self.con.execute(query, [message_time])
         self.con.commit()
-
-    def insert_new_warn(self, chat_id: int, user_id: int, message_time: int) -> None:
-        """Insert a new warn into the database"""
-        query = """
-        INSERT INTO warn_table (
-          ChatID,
-          UserID,
-          WarnDate
-        ) VALUES (?, ?, ?)
-        """
-        cur = self.con.cursor()
-        cur.execute(query, [chat_id, user_id, message_time])
-
-    def get_warns_since_timestamp(
-        self, chat_id: int, user_id: int, timestamp: int
-    ) -> int:
-        """Get the count of warns since timestamp"""
-        query = """
-        SELECT
-          ChatID,
-          UserID,
-          WarnDate,
-          COUNT(*)
-        FROM warn_table
-        WHERE ChatID = ?
-        AND UserID = ?
-        AND WarnDate >= ?
-        """
-        cur = self.con.cursor()
-        cur.execute(query, [chat_id, user_id, timestamp])
-        data = cur.fetchall()
-        if not data:
-            return 0
-        return data[0][3]
 
     def get_count_messages_sent_in_range(
         self, begin: int, end: int, chat_id: int
