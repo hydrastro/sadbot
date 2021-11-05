5dc6efeeb47059423f65d3dddc8e2f7bab1aeaea
meguminloli <meguminloli@protonmail.com>
2021-11-05 17:30:45 +0200

    added warn command

---
+++ sadbot/commands/warn.py
@@ -0,0 +1,76 @@
+"""Warn bot command"""
+import time
+from typing import List, Optional
+
+from sadbot.app import App, CHAT_MEMBER_STATUS_ADMIN, CHAT_MEMBER_STATUS_CREATOR
+from sadbot.bot_action import BOT_ACTION_TYPE_REPLY_TEXT, BotAction
+from sadbot.command_interface import BOT_HANDLER_TYPE_MESSAGE, CommandInterface
+from sadbot.message import Message
+from sadbot.message_repository import MessageRepository
+
+
+class WarnBotCommand(CommandInterface):
+    """This is the warn command class"""
+
+    def __init__(self, app: App, message_repository: MessageRepository):
+        """Initializes the warn command"""
+        self.app = app
+        self.message_repository = message_repository
+
+    @property
+    def handler_type(self) -> int:
+        """Returns the type of event handled by the command"""
+        return BOT_HANDLER_TYPE_MESSAGE
+
+    @property
+    def command_regex(self) -> str:
+        """Returns the regex string that triggers this command"""
+        return "([.]|[!]|[/])[wW][aA][rR][nN]"
+
+    def get_reply(self, message: Optional[Message] = None) -> Optional[List[BotAction]]:
+        """Returns the command output"""
+        if message is None or message.text is None:
+            return None
+        if message.reply_id is not None:
+            warn_message = self.message_repository.get_reply_message(message)
+            if warn_message is None:
+                return None
+            warn_sender_id = warn_message.sender_id
+            warn_username = warn_message.sender_username
+        else:
+            text = message.text.split()
+            if len(text) < 2:
+                return None
+            warn_username = text[1].replace("@", "")
+            warn_sender_id = self.message_repository.get_user_id_from_username(
+                warn_username
+            )
+            if warn_sender_id is None:
+                return None
+        if warn_sender_id is None:
+            return None
+        user_permissions = self.app.get_user_status_and_permissions(
+            message.chat_id, message.sender_id
+        )
+        if user_permissions is None:
+            return None
+        user_type = user_permissions[0]
+        if user_type not in [CHAT_MEMBER_STATUS_ADMIN, CHAT_MEMBER_STATUS_CREATOR] or (
+            user_type != CHAT_MEMBER_STATUS_CREATOR
+            and not user_permissions[1].can_restrict_members
+        ):
+            return [
+                BotAction(
+                    BOT_ACTION_TYPE_REPLY_TEXT,
+                    reply_text="You don't have enough rights to mute, kiddo.",
+                )
+            ]
+        timestamp = int(time.time()) - 604800
+        count = self.message_repository.get_warns_since_timestamp(
+            message.chat_id, warn_sender_id, timestamp
+        )
+        reply_text = f"{warn_username} has been successfully warned. The user has {count} {'warns' if count != 1 else 'warn'} in the last week"
+        self.message_repository.insert_new_warn(
+            message.chat_id, warn_sender_id, int(time.time())
+        )
+        return [BotAction(BOT_ACTION_TYPE_REPLY_TEXT, reply_text=reply_text)]
