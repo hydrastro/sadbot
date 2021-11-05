ac4da5b23f2f3f8cdcab8ca2bc5626f89675c7db
meguminloli <meguminloli@protonmail.com>
2021-11-05 18:46:12 +0200

    added mute in case of warnings

--- sadbot/commands/warn.py
+++ sadbot/commands/warn.py
@@ -1,21 +1,50 @@
 """Warn bot command"""
 import time
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from sadbot.app import App, CHAT_MEMBER_STATUS_ADMIN, CHAT_MEMBER_STATUS_CREATOR
-from sadbot.bot_action import BOT_ACTION_TYPE_REPLY_TEXT, BotAction
+from sadbot.bot_action import (
+    BOT_ACTION_PRIORITY_HIGH,
+    BOT_ACTION_TYPE_REPLY_TEXT,
+    BOT_ACTION_TYPE_RESTRICT_CHAT_MEMBER,
+    BotAction,
+)
+from sadbot.chat_permissions import ChatPermissions
+from sadbot.classes.permissions import Permissions
 from sadbot.command_interface import BOT_HANDLER_TYPE_MESSAGE, CommandInterface
 from sadbot.message import Message
 from sadbot.message_repository import MessageRepository
 
 
+def mute_time(count: int) -> Optional[Tuple[int, str]]:
+    """Mute time for current count"""
+    # <3 - no mute
+    # 3 - 4 hours
+    # 4 - 1 day
+    # 5 - 3 days
+    # >5 - permamute
+    if count < 3:
+        return None
+    elif count == 3:
+        return (14400, "4 hours")
+    elif count == 4:
+        return (86400, "1 day")
+    elif count == 5:
+        return (259200, "3 days")
+    else:
+        return (0, "eternity")
+
+
 class WarnBotCommand(CommandInterface):
     """This is the warn command class"""
 
-    def __init__(self, app: App, message_repository: MessageRepository):
+    def __init__(
+        self, app: App, message_repository: MessageRepository, permissions: Permissions
+    ):
         """Initializes the warn command"""
         self.app = app
         self.message_repository = message_repository
+        self.permissions = permissions
 
     @property
     def handler_type(self) -> int:
@@ -25,11 +54,9 @@ class WarnBotCommand(CommandInterface):
     @property
     def command_regex(self) -> str:
         """Returns the regex string that triggers this command"""
-        return "([.]|[!]|[/])[wW][aA][rR][nN]"
+        return "([.]|[!]|[/])[wW][aA][rR][nN].*"
 
-    def get_reply(
-        self, message: Optional[Message] = None
-    ) -> Optional[List[BotAction]]:
+    def get_reply(self, message: Optional[Message] = None) -> Optional[List[BotAction]]:
         # pylint: disable=R0911
         """Returns the command output"""
         if message is None or message.text is None:
@@ -48,6 +75,7 @@ class WarnBotCommand(CommandInterface):
             warn_username = text[1].replace("@", "")
             if warn_username is None:
                 return None
+            print(warn_username)
             warn_sender_id = self.message_repository.get_user_id_from_username(
                 warn_username
             )
@@ -70,12 +98,52 @@ class WarnBotCommand(CommandInterface):
                 )
             ]
         timestamp = int(time.time()) - 604800
-        count = self.message_repository.get_warns_since_timestamp(
-            message.chat_id, warn_sender_id, timestamp
-        )
-        reply_text = f"""{warn_username} has been successfully warned.
-        The user has {count} {'warns' if count != 1 else 'warn'} in the last week"""
         self.message_repository.insert_new_warn(
             message.chat_id, warn_sender_id, int(time.time())
         )
-        return [BotAction(BOT_ACTION_TYPE_REPLY_TEXT, reply_text=reply_text)]
+        count = self.message_repository.get_warns_since_timestamp(
+            message.chat_id, warn_sender_id, timestamp
+        )
+        mute = mute_time(count)
+        if mute is None:
+            reply_text = (
+                f"{warn_username} has been successfully warned.\n"
+                + f"The user has {count} {'warns' if count != 1 else 'warn'} in the last week"
+            )
+            return [
+                BotAction(
+                    BOT_ACTION_TYPE_REPLY_TEXT,
+                    reply_text=reply_text,
+                    reply_priority=BOT_ACTION_PRIORITY_HIGH,
+                )
+            ]
+        else:
+            reply_text = (
+                f"{'User' if warn_username is None else warn_username} has been successfully warned.\n"
+                + f"The user has {count} {'warns' if count != 1 else 'warn'} in the last week.\n"
+                + f"Because of the warnings, you are muted for {mute[1]}"
+            )
+            mute_permissions = ChatPermissions(
+                False, False, False, False, False, False, False, False
+            )
+            until_date = int(time.time() + mute[0])
+            print(until_date)
+            mute_permissions.ban_until_date = until_date
+            print(warn_sender_id, message.chat_id, mute_permissions)
+            self.permissions.set_user_permissions(
+                warn_sender_id, message.chat_id, mute_permissions
+            )
+            return [
+                BotAction(
+                    BOT_ACTION_TYPE_RESTRICT_CHAT_MEMBER,
+                    reply_ban_user_id=warn_sender_id,
+                    reply_permissions=mute_permissions,
+                    reply_restrict_until_date=until_date,
+                    reply_priority=BOT_ACTION_PRIORITY_HIGH,
+                ),
+                BotAction(
+                    BOT_ACTION_TYPE_REPLY_TEXT,
+                    reply_text=reply_text,
+                    reply_priority=BOT_ACTION_PRIORITY_HIGH,
+                ),
+            ]
