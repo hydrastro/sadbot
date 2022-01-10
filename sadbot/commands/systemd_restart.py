7be92f6ef0afdc199aeed6dd2f44953e4fa41bf7
hydrastro <9256943+hydrastro@users.noreply.github.com>
2022-01-10 08:19:55 +0100

    Create systemd_restart.py (#35)
    * Create systemd_restart.py
    
    * Update systemd_restart.py

---
+++ sadbot/commands/systemd_restart.py
@@ -0,0 +1,28 @@
+"""Systemd service restart bot command"""
+
+import random
+from typing import Optional, List
+import os
+
+from sadbot.command_interface import CommandInterface, BOT_HANDLER_TYPE_MESSAGE
+from sadbot.message import Message
+from sadbot.bot_action import BotAction, BOT_ACTION_TYPE_NONE
+
+class SystemdRestartBotCommand(CommandInterface):
+    """This is the restart bot command class"""
+
+    @property
+    def handler_type(self) -> int:
+        """Returns the type of event handled by the command"""
+        return BOT_HANDLER_TYPE_MESSAGE
+
+    @property
+    def command_regex(self) -> str:
+        """Returns the regex for matching the roll command"""
+        return r"(\.[Rr][Ee][Ss][Tt][Aa][Rr][Tt]{2}).*"
+
+    def get_reply(self, message: Optional[Message] = None) -> Optional[List[BotAction]]:
+        """Restarts the bot systemd service"""
+        # Remember to allow this command in /etc/sudoers
+        os.system("sudo service sadbot restart")
+        return [BotAction(BOT_ACTION_TYPE_NONE)]
