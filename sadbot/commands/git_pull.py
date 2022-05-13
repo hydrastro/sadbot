9622d5903ccc88feceaaa6eebecf47820161691f
hydrastro <9256943+hydrastro@users.noreply.github.com>
2022-05-14 00:01:51 +0200

    uwu

--- sadbot/commands/git_pull.py
+++ sadbot/commands/git_pull.py
@@ -6,7 +6,8 @@ import subprocess
 from sadbot.command_interface import CommandInterface, BOT_HANDLER_TYPE_MESSAGE
 from sadbot.message import Message
 from sadbot.bot_action import BotAction, BOT_ACTION_TYPE_REPLY_TEXT
-from sadbot.config import OWNER_ID
+
+# from sadbot.config import OWNER_ID
 
 
 class GitPullBotCommand(CommandInterface):
@@ -26,8 +27,9 @@ class GitPullBotCommand(CommandInterface):
         """Restarts the bot systemd service"""
         if message is None:
             return None
-        if message.sender_id != OWNER_ID:
-            reply_text = "No."
-            return [BotAction(BOT_ACTION_TYPE_REPLY_TEXT, reply_text=reply_text)]
+        # uwu
+        # if message.sender_id != OWNER_ID:
+        #    reply_text = "No."
+        #    return [BotAction(BOT_ACTION_TYPE_REPLY_TEXT, reply_text=reply_text)]
         result = subprocess.check_output("git pull", shell=True, encoding="UTF-8")
         return [BotAction(BOT_ACTION_TYPE_REPLY_TEXT, reply_text=result)]
