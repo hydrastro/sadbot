cde9af092db5aa0df99a5f843b81ddba74351048
sneedcat <basedsneedcat@proton.me>
2023-10-18 10:42:12 +0300

    fixed spoiler command

--- sadbot/commands/spoiler.py
+++ sadbot/commands/spoiler.py
@@ -1,7 +1,6 @@
 """Spoiler bot command"""
 
 from typing import Optional, List
-import requests
 from sadbot.app import App
 
 from sadbot.command_interface import CommandInterface, BOT_HANDLER_TYPE_MESSAGE
@@ -15,8 +14,8 @@ from sadbot.bot_action import (
     BotAction,
     BOT_ACTION_TYPE_DELETE_MESSAGE,
     BOT_ACTION_TYPE_REPLY_VIDEO,
+    BOT_ACTION_TYPE_REPLY_TEXT,
     BOT_ACTION_TYPE_REPLY_IMAGE,
-    BOT_ACTION_TYPE_NONE,
 )
 
 
@@ -43,22 +42,40 @@ class SpoilerBotCommand(CommandInterface):
         if message is None or message.reply_id is None or message.text is None:
             return None
         reply_message = self.message_repository.get_reply_message(message)
-        if reply_message is None or reply_message.file_id is None:
+        if reply_message is None:
             return None
         file_bytes = self.app.get_file_from_id(reply_message.file_id)
         action = None
+        sender = reply_message.sender_name
+        if reply_message.sender_username:
+            sender = f"@{reply_message.sender_username}"
+        reply_text = f'Sender: {sender}'
+        if reply_message.text:
+            reply_text += f'\n<span class="tg-spoiler">{reply_message.text}</span>'
         if reply_message.file_type == MESSAGE_FILE_TYPE_PHOTO:
             action = BotAction(
-                BOT_ACTION_TYPE_REPLY_IMAGE, reply_image=file_bytes, reply_spoiler=True
+                BOT_ACTION_TYPE_REPLY_IMAGE,
+                reply_image=file_bytes,
+                reply_spoiler=True,
+                reply_text=reply_text,
+                reply_text_parse_mode="HTML",
             )
         elif reply_message.file_type == MESSAGE_FILE_TYPE_VIDEO:
             action = BotAction(
-                BOT_ACTION_TYPE_REPLY_VIDEO, reply_video=file_bytes, reply_spoiler=True
+                BOT_ACTION_TYPE_REPLY_VIDEO,
+                reply_video=file_bytes,
+                reply_spoiler=True,
+                reply_text=reply_text,
+                reply_text_parse_mode="HTML",
             )
         else:
             return None
         if file_bytes is None:
-            return None
+            action = BotAction(
+                BOT_ACTION_TYPE_REPLY_TEXT,
+                reply_text=reply_text,
+                reply_text_parse_mode="HTML",
+            )
         return [
             BotAction(
                 BOT_ACTION_TYPE_DELETE_MESSAGE,
